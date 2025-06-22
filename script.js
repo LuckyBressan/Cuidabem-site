b15c2cc18f853d5425849c7dee8f1501abd9ff92
LuckyBressan <lucas.a.dos.s.bressan@gmail.com>
2025-06-22 11:19:33 -0300

    feat: ajustes de sections + adição de logo + mudança do ícone da seção inicial;

--- script.js
+++ script.js
@@ -29,9 +29,32 @@
         });
 
         // Form Submission (Demo)
-        const contactForm = document.querySelector('form');
-        contactForm.addEventListener('submit', function(e) {
-            e.preventDefault();
-            alert('Obrigado por entrar em contato! Sua mensagem foi enviada com sucesso. (Esta é uma demonstração)');
-            contactForm.reset();
+        // const contactForm = document.querySelector('form');
+        // contactForm.addEventListener('submit', function(e) {
+        //     e.preventDefault();
+        //     alert('Obrigado por entrar em contato! Sua mensagem foi enviada com sucesso. (Esta é uma demonstração)');
+        //     contactForm.reset();
+        // });
+
+        //seja um cuidador
+        document.querySelectorAll('.button-seja-cuidador').forEach(button => {
+            button.addEventListener('click', () => {
+                const link = document.createElement('a');
+                link.href = 'https://forms.gle/oXuuqNs4JYT76Qyi9';
+                link.target = '_blank';
+                link.style.display = 'none'; // Hide the link element
+                document.body.appendChild(link); // Append to body
+                link.click();
+            });
+        });
+        //Encontre um cuidador
+        document.querySelectorAll('.button-encontrar-cuidador').forEach(button => {
+            button.addEventListener('click', () => {
+                const link = document.createElement('a');
+                link.href = 'https://forms.gle/n89CFwHbUKkYUvq59';
+                link.target = '_blank';
+                link.style.display = 'none'; // Hide the link element
+                document.body.appendChild(link); // Append to body
+                link.click();
+            });
         });
